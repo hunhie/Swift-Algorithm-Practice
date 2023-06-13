7c406a2c453cb8dbc619cfd6657fe19a85d9e1cb
HUNHIE LEE <huny98.dev@gmail.com>
2023-06-13 13:44:20 +0900

    [unrated] Title: 달리기 경주, Time: 0.15 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.swift
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.swift
@@ -1,27 +1,21 @@
 import Foundation
 
 func solution(_ players:[String], _ callings:[String]) -> [String] {
+  // 선수 배열의 사본
   var arr = players
-  var indices: [String: Int] = [:]  // 요소의 인덱스를 저장할 딕셔너리
+  // 선수 이름과 인덱스 순서가 담긴 딕셔너리 생성
+  var dict: [String: Int] = Dictionary(uniqueKeysWithValues: zip(players, 0..<players.count))
   
-  for (index, player) in players.enumerated() {
-    indices[player] = index // 각 요소의 인덱스를 딕셔너리에 저장
+  // 호출된 선수와 추월 당할 선수의 순서, 인덱스 교체
+  callings.forEach { calling in
+    let currentPlayer = dict[calling]!
+    let previousePlayer = arr[currentPlayer - 1]
+    arr.swapAt(currentPlayer, currentPlayer - 1)
+    
+    dict[calling]! -= 1
+    dict[previousePlayer]! += 1
   }
   
-  for calling in callings {
-    if let currentIndex = indices[calling] {    // 호출된 플레이어의 index를 딕셔너리에서 조회
-      let previousIndex = currentIndex - 1      // 호출된 플레이어가 추월한 플레이어의 index 계산
-      let currentPlayer = arr[currentIndex]     // 호출된 플레이어의 문자열
-      let previousePlayer = arr[previousIndex]  // 추월 당한 플레이어의 문자열
-      
-      // 호출된 플레이어와 추월 당한 플레이어의 순서, index 교체
-      arr[currentIndex] = previousePlayer
-      arr[previousIndex] = currentPlayer
-      
-      indices[currentPlayer] = previousIndex
-      indices[previousePlayer] = currentIndex
-    }
-  }
-  
+  // 새로운 순위 배열 반환
   return arr
 }
