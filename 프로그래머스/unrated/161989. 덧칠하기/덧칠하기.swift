9da2170e2483a216925a77eae620246b0009c8ee
HUNHIE LEE <huny98.dev@gmail.com>
2023-07-17 10:39:11 +0900

    [unrated] Title: 덧칠하기, Time: 5.38 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.swift
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.swift
@@ -1,16 +1,22 @@
 import Foundation
 
 func solution(_ n:Int, _ m:Int, _ section:[Int]) -> Int {
-    // 칠한 횟수
+    // 페인트칠한 횟수
     var count: Int = 1
-    // 마지막으로 칠한 시작 인덱스
+    
+    // 마지막으로 페인트칠한 인덱스
     var start: Int = section[0]
     
     for i in section {
-        // 현재 인덱스의 페인트칠 여부 확인
+        // 현재 인덱스가 마지막으로 페인트칠한 범위에 속해 있다면 다음 인덱스로 건너뛰기
         guard start + m <= i else { continue }
-        start = i
+        
+        // 칠하지 않은 인덱스라면 페인트 칠하기
         count += 1
+        
+        // 마지막으로 칠한 인덱스 기록
+        start = i
     }
+    
     return count
 }
